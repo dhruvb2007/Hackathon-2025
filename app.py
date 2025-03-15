d66364598d3f40b6531beebe70794f719c12e4cd
Dhurv Prajapati <159237463+dhruvb2007@users.noreply.github.com>
2025-03-15 19:52:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from routes.predict import predict_bp
 from routes.personalized_pathway import pathway_bp
@@ -9,4 +10,5 @@ app.register_blueprint(predict_bp)
 app.register_blueprint(pathway_bp)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if no PORT is set
+    app.run(debug=True, host="0.0.0.0", port=port)
