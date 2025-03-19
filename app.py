fa83aa195a688529c72108f52e13f311ac6a291d
Dhurv Prajapati <159237463+dhruvb2007@users.noreply.github.com>
2025-03-19 21:07:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from routes.predict import predict_bp
 from routes.personalized_pathway import pathway_bp
 
 app = Flask(__name__)
-CORS(app)  # Enable CORS for all routes
+CORS(app, resources={r"/*": {"origins": "*"}})   # Enable CORS for all routes
 
 # Register blueprints
 app.register_blueprint(predict_bp)
