02760eae8b9e75bac715948efac265ed6a51f088
Dhurv Prajapati <159237463+dhruvb2007@users.noreply.github.com>
2025-03-20 08:45:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
+import os
 from flask import Flask
 from flask_cors import CORS
 from routes.predict import predict_bp
 from routes.personalized_pathway import pathway_bp
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})   # Enable CORS for all routes
+CORS(app, resources={r"/*": {"origins": "*"}})  # Enable CORS for all routes
 
 # Register blueprints
 app.register_blueprint(predict_bp)
 app.register_blueprint(pathway_bp)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use Railway's assigned port
+    app.run(host="0.0.0.0", port=port, debug=True)
